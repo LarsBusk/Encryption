b964300e6dc6746430687baa758dafc55d3e04f9
Lars Busk <lab@foss.dk>
2017-10-14 21:56:28 +0200

    Added optionm for decrypting an entire export

---
+++ EncryptDecrypt/EncryptDecrypt/DataFileHelper.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace EncryptDecrypt
+{
+  public class DataFileHelper
+  {
+    public List<SampleContent> GetSamples(string dataFileName)
+    {
+      List<SampleContent> samples = new List<SampleContent>();
+
+      XDocument data = XDocument.Load(dataFileName);
+      XNamespace ns = "http://foss.dk/Mosaic/Export";
+
+      XElement sampleGroups = data.Element(ns + "Instruments").Element(ns + "Instrument").Element(ns + "SampleGroups");
+
+      var sampleElements = sampleGroups.Elements(ns + "SampleGroup").Elements(ns + "SampleList").Elements(ns + "Sample");
+      int sampleNumber = 0;
+      foreach (var sampleElement in sampleElements)
+      {
+        sampleNumber++;
+        SampleContent sampleContent = new SampleContent(sampleNumber);
+
+        var rawDataElements = sampleElement.Element(ns + "RawValueList").Elements(ns + "RawValue");
+
+        foreach (var rawDataElement in rawDataElements)
+        {
+          RawDataContent rawDataContent = new RawDataContent(rawDataElement.Attribute("Identification").Value,
+            rawDataElement.Element(ns + "RawDataFile").Attribute("FileName").Value,
+            rawDataElement.Element(ns + "RawDataFile").Attribute("PathName").Value,
+            false);
+
+          sampleContent.AddRawData(rawDataContent);
+        }
+
+        samples.Add(sampleContent);
+
+        var subSampliList = sampleElement.Element(ns + "SubSampleList");
+
+        if (subSampliList != null)
+        {
+          var subSamples = subSampliList.Elements(ns + "SubSample");
+          int subSampleNumber = 0;
+
+          foreach (var subSample in subSamples)
+          {
+            subSampleNumber++;
+            SampleContent subSampleContent = new SampleContent(sampleNumber, true , subSampleNumber);
+            var subSampleRawDatas = subSample.Elements(ns + "RawValueList").Elements(ns + "RawValue");
+
+            foreach (var subSampleRawData in subSampleRawDatas)
+            {
+              RawDataContent subSamplerawDataContent = new RawDataContent(subSampleRawData.Attribute("Identification").Value,
+                subSampleRawData.Element(ns + "RawDataFile").Attribute("FileName").Value,
+                subSampleRawData.Element(ns + "RawDataFile").Attribute("PathName").Value,
+                false);
+              subSampleContent.AddRawData(subSamplerawDataContent);
+            }
+
+            samples.Add(subSampleContent);
+          }
+        }
+      }
+
+      return samples;
+    }
+  }
+}
---
+++ EncryptDecrypt/EncryptDecrypt/SampleContent.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EncryptDecrypt
+{
+  public class SampleContent
+  {
+    public int SampleNumber { get; set; }
+    public bool IsSubSample { get; set; }
+    public int SubSampleNumber { get; set; }
+    public List<RawDataContent> RawDataContents { get; private set; }
+
+    public SampleContent(int sampleNumber,bool isSubSample = false, int subSampleNumber = 0)
+    {
+      RawDataContents = new List<RawDataContent>();
+
+      this.SampleNumber = sampleNumber;
+      this.IsSubSample = isSubSample;
+      this.SubSampleNumber = subSampleNumber;
+    }
+
+    public void AddRawData(RawDataContent rawDataContent)
+    {
+      if (RawDataContents.Any(r => r.Identification.Equals(rawDataContent.Identification)))
+      {
+        rawDataContent.RawDataNumber++;
+      }
+
+      RawDataContents.Add(rawDataContent);
+    }
+
+    public override string ToString()
+    {
+      return this.IsSubSample ? $"Sample_{SampleNumber}_SubSample" : $"Sample_{SampleNumber}";
+    }
+  }
+}
