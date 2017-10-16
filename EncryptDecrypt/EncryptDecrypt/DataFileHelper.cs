25e353377d9c5836fad24250efb1dd898f9529b1
Lars Busk <lab@foss.dk>
2017-10-16 15:09:32 +0200

    Small improvements

--- EncryptDecrypt/EncryptDecrypt/DataFileHelper.cs
+++ EncryptDecrypt/EncryptDecrypt/DataFileHelper.cs
@@ -8,23 +8,29 @@ using System.Xml.Linq;
 
 namespace EncryptDecrypt
 {
+  /// <summary>
+  /// Class that gets information about rawdata from samples and subsamples and settings files
+  /// from a data export file. The data must be in a subfolder.
+  /// </summary>
   public class DataFileHelper
   {
-    private XNamespace ns = "http://foss.dk/Mosaic/Export";
+    private readonly XNamespace ns = "http://foss.dk/Mosaic/Export";
 
-    private XDocument data;
-
-    private XElement sampleGroups;
+    private readonly XElement sampleGroups;
 
     public DataFileHelper(string dataFileName)
     {
-      data = XDocument.Load(dataFileName);
+      var data = XDocument.Load(dataFileName);
       sampleGroups = data.Element(ns + "Instruments").Element(ns + "Instrument").Element(ns + "SampleGroups");
     }
 
+    /// <summary>
+    /// Gets rawdata from samples and subsamples.
+    /// </summary>
+    /// <returns>A list of sample contents</returns>
     public List<SampleContent> GetSamples()
     {
-      List<SampleContent> samples = new List<SampleContent>();
+      var samples = new List<SampleContent>();
 
       var sampleElements = sampleGroups.Elements(ns + "SampleGroup").Elements(ns + "SampleList").Elements(ns + "Sample");
       int sampleNumber = 0;
@@ -68,12 +74,17 @@ namespace EncryptDecrypt
       return samples;
     }
 
+    /// <summary>
+    /// Gets a list of files with settings.
+    /// </summary>
+    /// <returns></returns>
     public List<Tuple<string, string>> SettingsFileList()
     {
       var settings = sampleGroups.Elements(ns + "SampleGroup").Elements(ns + "InstrumentExtensionList")
         .Elements(ns + "InstrumentExtension")
         .Elements(ns + "SettingGroup").Elements(ns + "Setting")
         .Where(s => s.Attribute("DataType").Value.Equals("File"));
+
       List<Tuple<string,string>> files = new List<Tuple<string, string>>();
 
       foreach (var setting in settings)
@@ -99,8 +110,7 @@ namespace EncryptDecrypt
     {
       return new RawDataContent(rawDataElement.Attribute("Identification").Value,
         rawDataElement.Element(ns + "RawDataFile").Attribute("FileName").Value,
-        rawDataElement.Element(ns + "RawDataFile").Attribute("PathName").Value,
-        false);
+        rawDataElement.Element(ns + "RawDataFile").Attribute("PathName").Value);
     }
   }
 }
